2d4e848fb9a8f1573ec03c8239f9c5ef669c5993
kendell <kendell.r@outlook.com>
2015-08-11 07:32:03 -0700

    Update ROASArdLib.cpp

--- ROAS-Ard-Lib/ROASArdLib.cpp
+++ ROAS-Ard-Lib/ROASArdLib.cpp
@@ -4,7 +4,25 @@
 int RoboPins[11];
 
 String IfRunning = "No";
+String IfDidSetup = "No";
+String IfInLoop = "No"
 
 _ROASClass::ROASBegin() {
   IfRunning = "Yes";
+}
+
+_ROASClass::beginsetup() {
+  IfDidSetup = "No";
+}
+
+_ROASClass::endsetup() {
+  IfDidSetup = "Yes";
+}
+
+_ROASClass::beginloop() {
+  IfInLoop = "FirstCommand";
+}
+
+_ROASClass::loopsetup() {
+  IfInLoop = "SecondCommand";
 }
