91e357fc08787023b086c0b4e8923d9e9c4a2dab
kendell <kendell.r@outlook.com>
2015-08-11 07:41:40 -0700

    Update ROASArdLib.h

--- ROAS-Ard-Lib/ROASArdLib.h
+++ ROAS-Ard-Lib/ROASArdLib.h
@@ -1,4 +1,17 @@
 #ifdef ROASARDLIB_h
 #define ROASARDLIB_h
 
+#include "Arduino.h"
+
+class _ROASClass
+{
+  void ROASBegin();
+  void beginsetup();
+  void endsetup();
+  void beginloop();
+  void loopsetup();
+  void endloop();
+  extern _ROASClass ROASArdLib;
+  extern _ROASClass ROASLib;
+}
 #endif
